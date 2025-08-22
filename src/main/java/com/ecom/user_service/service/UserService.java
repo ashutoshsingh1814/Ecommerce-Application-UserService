143b282c3cb5ec6faf17a6444433b96507665c31
ashutoshsingh1814 <ashutoshsingh1814@gmail.com>
2025-08-22 22:56:11 +0530

    4th commit to Dev Branch 20-08-25

--- src/main/java/com/ecom/user_service/service/UserService.java
+++ src/main/java/com/ecom/user_service/service/UserService.java
@@ -1,6 +1,8 @@
 package com.ecom.user_service.service;
 
 import com.ecom.user_service.dto.UserDto;
+import org.springframework.stereotype.Service;
+
 
 public interface UserService {
 
--- src/main/java/com/ecom/user_service/service/UserServiceImpl.java
+++ src/main/java/com/ecom/user_service/service/UserServiceImpl.java
@@ -1,21 +1,51 @@
 package com.ecom.user_service.service;
 
 import com.ecom.user_service.dto.UserDto;
+import com.ecom.user_service.entity.Credential;
+import com.ecom.user_service.entity.User;
+import com.ecom.user_service.mapper.UserMapper;
+import com.ecom.user_service.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl  implements UserService{
 
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private UserMapper userMapper;
     @Override
     public UserDto saveUser(UserDto userDto) {
-        return null;
+
+        User user = userMapper.toEntity(userDto);
+        Credential credential = user.getCredential();
+
+        // TO DO: As of now we are providing original password to DB, but we need to store encoded password.
+        //BiDirectional.
+        credential.setUser(user);
+        User dbUser = userRepo.save(user);
+
+        return userMapper.toDto(dbUser);
     }
 
     @Override
     public UserDto findById(Integer UserId) {
-        return null;
+        return userRepo.findById(UserId).map(userMapper::toDto)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + UserId));
     }
 
     @Override
     public UserDto update(Integer userId, UserDto userDto) {
-        return null;
+    User existingUser = userRepo.findById(userId)
+            .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+
+             existingUser.setFirstName(userDto.getFirstName());
+             existingUser.setLastName(userDto.getLastName());
+             existingUser.setEmail(userDto.getEmailAddress());
+             existingUser.setPhone(userDto.getContact());
+
+             User updatedUser = userRepo.save(existingUser);
+        return userMapper.toDto(updatedUser);
     }
 }
